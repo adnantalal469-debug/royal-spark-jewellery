96f7e3dd46c5ef059e0d3fe14598d534445688f2
adnantalal469-debug <adnantalal469@gmail.com>
2026-05-29 20:36:44 +0300

    Create product.js

---
+++ product.js
@@ -0,0 +1,23 @@
+const products = [
+
+{
+id:1,
+name:"Luxury Ring",
+price:"79 QAR",
+reviews:"4.9 ★",
+delivery:"2-4 Days",
+image:"https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?q=80&w=1200",
+description:"Premium VIP artificial ring."
+},
+
+{
+id:2,
+name:"Diamond Necklace",
+price:"149 QAR",
+reviews:"5.0 ★",
+delivery:"3-5 Days",
+image:"https://images.unsplash.com/photo-1617038220319-276d3cfab638?q=80&w=1200",
+description:"Luxury diamond necklace set."
+}
+
+];
